8382881a8be23374e2b10dbb164fb7562766c651
mikamikann <takamugi9924@gmail.com>
2026-05-05 09:23:37 +0900

    ・定期実行（TaskBatchService）でnotifications テーブルにデータが作成されたタスクの削除時、 notifications 側にデータが残っているため、データベースの拒否のエラーが出た。 →リレーション設定追加で、親子まとめて削除する設定を追加 ・表示項目の並び順変更

--- src/main/java/com/m_takahisa/taskapp/task/Task.java
+++ src/main/java/com/m_takahisa/taskapp/task/Task.java
@@ -1,6 +1,7 @@
 package com.m_takahisa.taskapp.task;
 
 import com.m_takahisa.taskapp.auth.User;
+import com.m_takahisa.taskapp.task.notification.Notification;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -9,6 +10,7 @@ import lombok.ToString;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "tasks")
@@ -25,6 +27,10 @@ public class Task {
     @JoinColumn(name = "user_id", nullable = false, columnDefinition = "BIGINT") //
     @ToString.Exclude // デバック時のエラー対策（動作には影響しない）
     private User user;
+
+    // 通知テーブルとのリレーション設定
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Notification> notifications;
 
     @NotBlank(message = "タイトルは必須です")
     @Size(max = 100, message = "タイトルは100文字以内で入力してください")
